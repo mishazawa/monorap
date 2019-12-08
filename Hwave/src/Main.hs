33b5e91b17a8dc20cf63dad1d54768e0b0652aca
mishazawa <macintoshkrug@gmail.com>
2019-12-08 23:13:42 +0200

    some functions

--- Hwave/src/Main.hs
+++ Hwave/src/Main.hs
@@ -49,3 +49,29 @@ gdc x 0 = abs x
 gdc x y = gdc b (mod a b)
   where a = abs x
         b = abs y
+
+ddrop 0 ls = ls
+ddrop _ [] = []
+ddrop n (_:xs) = pred n `ddrop` xs
+
+llen [] = 0
+llen (_:xs) = 1 + llen xs
+
+ttake 0 _ = []
+ttake _ [] = []
+ttake n (x:xs) = x:rest 
+  where rest = pred n `ttake` xs
+
+ccycle [] = error "empty list"
+ccycle (x:xs) = first:rest
+  where first = x
+        rest = ccycle (xs ++ [x])
+
+
+rreverse [] = []
+rreverse (x:xs) = (++) (rreverse xs) [x]
+
+fastFib n _ 0 = n
+fastFib n o c = fastFib o (n + o) (pred c)
+
+ffib n = fastFib 0 1 n
