a7ebbcc5972f10812ba8ed538678cc6c2fb687e4
mishazawa <macintoshkrug@gmail.com>
2020-03-03 22:00:37 +0200

    red line

--- vufsidsdc/src/main/scala/example/Index.scala
+++ vufsidsdc/src/main/scala/example/Index.scala
@@ -3,14 +3,21 @@ package example
 import processing.core.{PApplet}
 
 class Index extends PApplet {
-      override def settings (): Unit = size(100, 100) 
+  override def settings (): Unit = size(Index.WIDTH, Index.HEIGHT)
 
-      override def draw() = {
-      
-      }
+  override def draw() = {
+    background(0)
+
+    // ground
+    stroke(255, 0, 0)
+    line(0, Index.HEIGHT / 2, Index.WIDTH, Index.HEIGHT / 2)
+  }
 }
 
 object Index {
-       def main(args: Array[String]) = PApplet.main("example.Index")
+  val WIDTH: Int = 200;
+  val HEIGHT: Int = 200;
+
+  def main(args: Array[String]) = PApplet.main("example.Index")
 }
 
